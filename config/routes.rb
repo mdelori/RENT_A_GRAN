e69452bcf75f5d01a8fc3aa66d5f0720a4ecdf62
Paul Schauss <p+github@schauss.fr>
2022-02-22 15:00:28 +0100

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-  resources :grannies, only: %i[index show new create]
-  resources :bookings, only: %i[index new create destroy]
-=======
+
   resources :grannies, only: %i[index show new create] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: %i[new create]
   end
-  resources :bookings, only: [:index, :destroy]
->>>>>>> e408a0a39f3e23f1e84e851ed32091ede145e010
+
+  resources :bookings, only: %i[index new create destroy]
 end
