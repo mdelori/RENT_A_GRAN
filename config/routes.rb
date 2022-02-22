426566410cd21b960350b1e1ffb114730e3ba900
Marie Delori <mariedelori@outlook.com>
2022-02-22 14:51:34 +0100

    new booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  resources :grannies, only: %i[index show new create]
-  resources :bookings, only: [:index, :new, :create, :destroy]
+  resources :grannies, only: %i[index show new create] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :destroy]
 end
