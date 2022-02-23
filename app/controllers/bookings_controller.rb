f9666c301838de2908aebab3f1836234869a3b95
Marie Delori <mariedelori@outlook.com>
2022-02-23 17:07:19 +0100

    üpdated shows for granny

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,10 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: %i[show destroy]
   before_action :set_granny, only: %i[new create]
+  before_action :set_user
 
   def index
-    @bookings = Booking.all
+    @bookings = @user.bookings
   end
 
   def show
@@ -38,6 +39,10 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:reservation_at, :granny_id, :user_id)
+  end
+
+  def set_user
+    @user = current_user
   end
 
 end
