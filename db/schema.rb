612d1071b8b79c872ebb88a06ab1c70167be085e
Paul Schauss <p+github@schauss.fr>
2022-02-23 12:01:28 +0100

    starting the geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_124843) do
+ActiveRecord::Schema.define(version: 2022_02_23_105916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2022_02_22_124843) do
     t.text "short_description"
     t.text "description"
     t.text "fun_fact"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_grannies_on_user_id"
   end
 
