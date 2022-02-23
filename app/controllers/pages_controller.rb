77fe5e2c9eaa1f62fbb13d753c1971765679a28c
Amine LAFI <aminelafi7@hotmail.com>
2022-02-23 12:25:40 +0100

    user: name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home, index]
 
   def home
     @grannies = Granny.all
+  end
+
+  def index
   end
 
   def profile
