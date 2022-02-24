437ace828c637ad80897334669ff5cb9aec4f247
Amine LAFI <aminelafi7@hotmail.com>
2022-02-24 13:55:04 +0100

    add price with surline blue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_143029) do
+ActiveRecord::Schema.define(version: 2022_02_24_105910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_143029) do
     t.string "country_name"
     t.float "latitude"
     t.float "longitude"
+    t.integer "price"
     t.index ["user_id"], name: "index_grannies_on_user_id"
   end
 
