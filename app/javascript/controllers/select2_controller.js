d59033b11741c089e349d7760655a18c830f149f
Alexandre Bouvier <alexandre.b2506@gmail.com>
2022-02-25 16:40:24 +0100

    fixes select css

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import $ from "jquery";
 import "select2";
-import 'select2/dist/css/select2.min.css';
 
 export default class extends Controller {
   connect() {
